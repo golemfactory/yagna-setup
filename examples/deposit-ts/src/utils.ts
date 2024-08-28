49641923921030f31be678af5fd002494e4d59bf
scx1332 <sieciech.czajka@golem.network>
2024-08-28 17:05:46 +0200

    Working on proper typescript implementation

--- examples/deposit-ts/src/utils.ts
+++ examples/deposit-ts/src/utils.ts
@@ -1,6 +1,6 @@
-import {readFile} from "fs/promises";
+import { readFile } from "fs/promises";
 
 export async function readJsonFile(path) {
-    const file = await readFile(path, "utf8");
-    return JSON.parse(file);
+  const file = await readFile(path, "utf8");
+  return JSON.parse(file);
 }
