ab2f167a4e675cb8d7b4127751a308a717250ec9
scx1332 <sieciech.czajka@golem.network>
2024-09-02 19:16:22 +0200

    refactored into observer

---
+++ examples/deposit-ts/src/observer.ts
@@ -0,0 +1,100 @@
+import { readJsonFile } from "./utils.js";
+import { Address, createPublicClient, decodeFunctionData, Hex, http, parseAbi, WatchEventReturnType } from "viem";
+import chalk from "chalk";
+import config from "./config.js";
+import { holesky } from "viem/chains";
+
+export interface ObserveTransactionsContext {
+    observedAddress: Address;
+    spenderAddress: Address | null;
+    unwatch: WatchEventReturnType;
+}
+
+const abiLock = await readJsonFile("./contracts/lockAbi.json");
+// publicClient for readContract functions
+const publicClient = createPublicClient({
+    chain: holesky,
+    transport: http(config.rpcUrl),
+});
+
+export function observeTransactionEvents(context: ObserveTransactionsContext): Promise<void> {
+    return new Promise((resolve) => {
+        context.unwatch = publicClient.watchEvent({
+            onLogs: async (logs) => {
+                const transactions = {};
+
+                for (const log of logs) {
+                    if (!(log.transactionHash in transactions)) {
+                        transactions[log.transactionHash] = [];
+                    }
+                    transactions[log.transactionHash].push(log);
+                }
+                let isResolved = false;
+                for (const txHash in transactions) {
+                    const transaction = await publicClient.getTransaction({ hash: <Hex>txHash });
+
+                    const parsedMethod = decodeFunctionData({
+                        abi: abiLock,
+                        data: transaction.input,
+                    });
+
+                    const logs = transactions[txHash];
+
+                    for (const log of logs) {
+                        const functionNamePlusArgs = `${parsedMethod.functionName}(${parsedMethod.args.join(", ")})`;
+                        console.log(chalk.magenta("\ncall:", functionNamePlusArgs));
+                        console.log(chalk.magenta("event:"), log.eventName);
+                        console.log(chalk.magenta("from:"), transaction.from);
+                        console.log(chalk.magenta("hash:"), transaction.hash, "\n");
+
+                        if (
+                            // if deposit is closed by our requestor, stop observing
+                            parsedMethod.functionName.toLowerCase().includes("close") &&
+                            transaction.from == context.observedAddress
+                        ) {
+                            isResolved = true;
+                        }
+
+                        if (
+                            // if deposit is terminated by our requestor, stop observing
+                            parsedMethod.functionName == "terminateDeposit" &&
+                            transaction.from == config.funder.address
+                        ) {
+                            isResolved = true;
+                        }
+                    }
+                }
+                if (isResolved) {
+                    context.unwatch();
+                    resolve();
+                }
+            },
+            events: parseAbi([
+                "event DepositCreated(uint256 indexed id, address spender)",
+                "event DepositClosed(uint256 indexed id, address spender)",
+                "event DepositExtended(uint256 indexed id, address spender)",
+                "event DepositFeeTransfer(uint256 indexed id, address spender, uint128 amount)",
+                "event DepositTerminated(uint256 indexed id, address spender)",
+                "event DepositTransfer(uint256 indexed id, address spender, address recipient, uint128 amount)",
+            ]),
+            address: <Hex>config.lockPaymentContract.holeskyAddress,
+        });
+    });
+}
+
+export async function spawnContractObserver() {
+    const context = {
+        observedAddress: <Address>config.lockPaymentContract.holeskyAddress,
+        spenderAddress: null,
+        unwatch: () => {
+            throw new Error("Cannot call unwatch before watch");
+        },
+    };
+
+    console.log(chalk.magenta(`Start observing Events on contract: ${context.observedAddress}`));
+    const observerFuture = observeTransactionEvents(context);
+    return {
+        context,
+        observerFuture,
+    };
+}
