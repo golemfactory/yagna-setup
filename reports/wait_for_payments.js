f2ae99a37fef50bc7d2b151399ab0bd9605ae3fd
scx1332 <sieciech.czajka@golem.network>
2024-09-27 16:21:32 +0200

    working on tests

--- reports/wait_for_payments.js
+++ reports/wait_for_payments.js
@@ -18,35 +18,39 @@ while (true) {
     let agreement_sum_scheduled = sum_of_scheduled(res.agreements);
     let agreement_sum_paid = sum_of_paid(res.agreements);
 
-    if (docs.length > 0) {
-        let total_sum = BigNumber(0);
-        let paid_sum = BigNumber(0);
-        docs.forEach(doc => {
-            total_sum = total_sum.plus(doc.amount);
-            if (doc.paid) {
-                paid_sum = paid_sum.plus(doc.amount);
-            }
-        })
-
-        console.log("Total order item amount: ", total_sum.toString());
-        console.log("Total order item paid amount: ", paid_sum.toString());
-        console.log("Total agreement scheduled amount: ", agreement_sum_scheduled.toString());
-        console.log("Total agreement accepted amount: ", agreement_sum_accepted.toString());
-        console.log("Total agreement paid amount: ", agreement_sum_paid.toString());
-
-        if (!total_sum.eq(paid_sum)) {
-            console.log("Items are not paid yet");
-            continue;
-        }
-        if (!agreement_sum_accepted.eq(agreement_sum_paid)) {
-            console.log("Agreements are not yet paid");
-            continue;
-        }
-        if (!total_sum.eq(agreement_sum_accepted)) {
-            console.log("Total sum of items is not equal to total sum of agreements");
-            continue;
-        }
-        console.log("All conditions are met, exiting");
-        break;
+    if (agreement_sum_accepted.eq(0)) {
+        console.log("No agreements yet");
+        continue;
     }
+
+    let total_sum = BigNumber(0);
+    let paid_sum = BigNumber(0);
+    docs.forEach(doc => {
+        total_sum = total_sum.plus(doc.amount);
+        if (doc.paid) {
+            paid_sum = paid_sum.plus(doc.amount);
+        }
+    })
+
+    console.log("Total order item amount: ", total_sum.toString());
+    console.log("Total order item paid amount: ", paid_sum.toString());
+    console.log("Total agreement scheduled amount: ", agreement_sum_scheduled.toString());
+    console.log("Total agreement accepted amount: ", agreement_sum_accepted.toString());
+    console.log("Total agreement paid amount: ", agreement_sum_paid.toString());
+
+    if (!total_sum.eq(paid_sum)) {
+        console.log("Items are not paid yet");
+        continue;
+    }
+    if (!agreement_sum_accepted.eq(agreement_sum_paid)) {
+        console.log("Agreements are not yet paid");
+        continue;
+    }
+    if (!total_sum.eq(agreement_sum_accepted)) {
+        console.log("Total sum of items is not equal to total sum of agreements");
+        continue;
+    }
+    console.log("All conditions are met, exiting");
+    break;
+
 }
