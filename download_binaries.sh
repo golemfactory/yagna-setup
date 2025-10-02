dad821a264145b98dc2d00b451d8701a3bc983a5
scx1332 <sieciech@gmail.com>
2025-10-02 17:25:13 +0200

    Update yagna version

--- download_binaries.sh
+++ download_binaries.sh
@@ -1,7 +1,7 @@
 set -x
 mkdir -p golem/downloaded
 cd golem/downloaded
-YAGNA_TAG=pre-rel-v0.17.0-payments-beta10
+YAGNA_TAG=pre-rel-v0.17.1-hoodi
 wget -qO- https://github.com/golemfactory/yagna/releases/download/${YAGNA_TAG}/golem-provider-linux-${YAGNA_TAG}.tar.gz | tar -xvz
 wget -qO- https://github.com/golemfactory/yagna/releases/download/${YAGNA_TAG}/golem-requestor-linux-${YAGNA_TAG}.tar.gz | tar -xvz
 
@@ -10,11 +10,11 @@ mv golem-requestor-linux-${YAGNA_TAG}/* .
 rm golem-provider-linux-${YAGNA_TAG} -r
 rm golem-requestor-linux-${YAGNA_TAG} -r
 
-wget -qO- https://github.com/golemfactory/ya-runtime-vm/releases/download/v0.3.0/ya-runtime-vm-linux-v0.3.0.tar.gz | tar -xvz
-mv ya-runtime-vm-linux-v0.3.0/* plugins/
-rm ya-runtime-vm-linux-v0.3.0 -r
+wget -qO- https://github.com/golemfactory/ya-runtime-vm/releases/download/v0.5.3/ya-runtime-vm-linux-v0.5.3.tar.gz | tar -xvz
+mv ya-runtime-vm-linux-v0.5.3/* plugins/
+rm ya-runtime-vm-linux-v0.5.3 -r
 
-wget -qO- https://github.com/golemfactory/ya-service-bus/releases/download/v0.7.2/ya-sb-router-linux-v0.7.2.tar.gz | tar -xvz
-mv ya-sb-router-linux-v0.7.2/* .
-rm ya-sb-router-linux-v0.7.2 -r
+wget -qO- https://github.com/golemfactory/ya-service-bus/releases/download/v0.7.4/ya-sb-router-linux-v0.7.4.tar.gz | tar -xvz
+mv ya-sb-router-linux-v0.7.4/* .
+rm ya-sb-router-linux-v0.7.4 -r
 
